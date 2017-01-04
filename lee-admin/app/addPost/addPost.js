e70a34ebee9a7d993e99458cdaa6548ccb14ce54
vascofornas <desarrollo@webjuarez.com>
2017-01-04 14:16:10 -0700

    Create WebApp Using AngularJS and Firebase - Creating Post (Part 11)

--- lee-admin/app/addPost/addPost.js
+++ lee-admin/app/addPost/addPost.js
@@ -9,6 +9,23 @@ angular.module ('myApp.addPost', ['ngRoute', 'firebase'])
     });
 }])
 
-.controller('AddPostCtrl', ['$scope', function($scope){
+.controller('AddPostCtrl', ['$scope', '$firebaseArray',function($scope, $firebaseArray){
+
+    var ref = firebase.database().ref().child('Enclaves');
+    $scope.articles = $firebaseArray(ref);
+
+    $scope.createPost = function(){
+        var nombre = $scope.article.nombre_enclave;
+        var descripcion = $scope.article.descripcion_enclave;
+        $scope.articles.$add({
+            
+            Nombre_enclave: nombre,
+            Descripcion_enclave: descripcion
+        }).then(function(ref){
+            console.log(ref);
+        }, function(error){
+            console.log(error);
+        });
+    };
 
 }]);
