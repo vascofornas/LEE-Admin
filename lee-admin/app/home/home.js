158ab00827a87313e4d06f2dc7b99210f3891381
vascofornas <desarrollo@webjuarez.com>
2017-01-03 23:45:40 -0700

    Create WebApp Using AngularJS and Firebase - Getting Started with Firebase (Part 5)

--- lee-admin/app/home/home.js
+++ lee-admin/app/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module ('myApp.home', ['ngRoute'])
+angular.module ('myApp.home', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/home', {
@@ -9,6 +9,19 @@ angular.module ('myApp.home', ['ngRoute'])
     });
 }])
 
-.controller('HomeCtrl', [function(){
-    
+.controller('HomeCtrl', ['$scope','$firebaseAuth', function($scope,$firebaseAuth){
+
+    $scope.signIn = function(){
+        var username = $scope.user.email;
+        var password = $scope.user.password;
+        var auth = $firebaseAuth();
+
+        auth.$signInWithEmailAndPassword(username,password).then(function(){
+            $scope.errMsg = false;
+        }).catch(function(error){
+            $scope.errMsg = true;
+            $scope.errorMessage = error.message;
+        });
+    }
+
 }])
